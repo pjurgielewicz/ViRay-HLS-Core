f7e95a8b9511e2a7e6cea96dc0995b300ab8d53a
Pawel Jurgielewicz <pjurgielewicz@o2.pl>
2017-12-12 16:39:21 +0100

    A bit of refactoring and further performance exploration -> peak theorethical performance @ 200 MHz : 190 MPx/s (~42500 cycles for 200x200)

--- Common/typedefs.h
+++ Common/typedefs.h
@@ -2,7 +2,6 @@
 #define TYPEDEFS__H_
 
 #define USE_FIXEDPOINT
-//#define USE_SMALLER_TYPES
 
 #ifdef USE_FIXEDPOINT
 
@@ -12,7 +11,6 @@
 
 #define FIXEDPOINT_WIDTH 32
 #define FIXEDPOINT_INTEGER_BITS 22
-//#define FIXEDPOINT_INTEGER_BITS_HALF 11
 
 #define PRAGMA_SUB(x) _Pragma (#x)
 #define DO_PRAGMA(x) PRAGMA_SUB(x)
@@ -21,7 +19,6 @@
 
 typedef ap_fixed<FIXEDPOINT_WIDTH, FIXEDPOINT_INTEGER_BITS, AP_RND> myType;
 //typedef float myType;
-typedef ap_fixed<25, 13, AP_RND>  myTypeReduced;
 
 typedef ap_fixed<16, 8, AP_RND> pixelColorType;
 
--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include "Utils/vec4.h"
-#include "Utils/mat4.h"
 #include "Utils/vision.h"
 
-void CreateRay(const CCamera& camera, const vec4& posShift, unsigned r, unsigned c, vec4* rayData)
+#define OBJ_NUM 10
+#define WIDTH 200
+#define HEIGHT 200
+#define FRAME_BUFFER_SIZE (HEIGHT)
+
+void CreateRay(const CCamera& camera, const vec4& posShift, unsigned r, unsigned c, CRay& ray)
 {
 #pragma HLS PIPELINE
+// TODO: Numerical errors may be probably reduced when values are normalized to the 'viewDistance'
+// TODO: Remove 'halfs' addition
+
 	vec4 samplePoint(posShift.data[0] + myType(0.5) + c,
 					 posShift.data[1] + myType(0.5) + r,
 					 0);
-	CRay ray = camera.GetCameraRayForPixel(samplePoint);
-	rayData[0] = ray.direction;
-	rayData[1] = ray.origin;
+	ray = camera.GetCameraRayForPixel(samplePoint);
 }
 
 void AssignMatrix(const vec4* mats, vec4* mat, unsigned pos)
@@ -24,57 +29,52 @@ void AssignMatrix(const vec4* mats, vec4* mat, unsigned pos)
 	}
 }
 
-void TransformRay(const vec4* mat, const vec4* rayData, vec4* transformedRayData)
+void TransformRay(const vec4* mat, const CRay& ray, CRay& transformedRay)
 {
 #pragma HLS PIPELINE
 #pragma HLS INLINE
-	// DIRECTION
-	transformedRayData[0].data[0] = mat[0].Dot3(rayData[0]);
-	transformedRayData[0].data[1] = mat[1].Dot3(rayData[0]);
-	transformedRayData[0].data[2] = mat[2].Dot3(rayData[0]);
 
-	// Transformation may change the magnitude
-//	transformedRayData[0] = transformedRayData[0].Normalize();
+	transformedRay.direction = 	vec4(mat[0].Dot3(ray.direction),
+								 	 mat[1].Dot3(ray.direction),
+									 mat[2].Dot3(ray.direction)).Normalize();
 
-	// ORIGIN
-	transformedRayData[1].data[0] = mat[0].Dot3(rayData[1]) + mat[0].data[3];
-	transformedRayData[1].data[1] = mat[1].Dot3(rayData[1]) + mat[1].data[3];
-	transformedRayData[1].data[2] = mat[2].Dot3(rayData[1]) + mat[2].data[3];
+	transformedRay.origin = 	vec4(mat[0].Dot3(ray.origin) + mat[0].data[3],
+								 	 mat[1].Dot3(ray.origin) + mat[1].data[3],
+									 mat[2].Dot3(ray.origin) + mat[2].data[3]);
 }
 
-myType SphereTest(const vec4* rayData)
+myType SphereTest(const CRay& transformedRay)
 {
 #pragma HLS PIPELINE
-	myType b = rayData[0].Dot3(rayData[1]);
-	myType c = myType(1) - rayData[1].Magnitude();
+// unit radius sphere in the global center
+	myType b = transformedRay.direction.Dot3(transformedRay.origin);
+	myType c = myType(1.0) - transformedRay.origin.Dot3(transformedRay.origin);
 	myType d2 = b * b - c;
 
-	if (d2 < myType(0))
+	if (d2 >= myType(0.0))
 	{
-		return myType(-1);
-	}
+		myType d = hls::sqrt(d2);
 
-	myType d = hls::sqrt(d2);
-
-	myType t = -b - d;
-	if (t > myType(0))
-	{
-		return t;
-	}
-	t = -b + d;
-	if (t > myType(0))
-	{
-		return t;
+		myType t = -b - d;
+		if (t > myType(0))
+		{
+			return t;
+		}
+		t = -b + d;
+		if (t > myType(0))
+		{
+			return t;
+		}
 	}
 
 	return myType(-1);
 }
 
-myType PlaneTest(const vec4* rayData)
+myType PlaneTest(const CRay& transformedRay)
 {
 #pragma HLS PIPELINE
 // XZ - plane pointing +Y
-	myType t = -rayData[1].data[1] * (myType(1) / rayData[0].data[1]);
+	myType t = -transformedRay.origin.data[1] * (myType(1) / transformedRay.direction.data[1]);
 	if (t > myType(0))
 	{
 		return t;
@@ -82,7 +82,7 @@ myType PlaneTest(const vec4* rayData)
 	return myType(-1);
 }
 
-myType PerformHits(const vec4* transformedRayData, unsigned objType)
+myType PerformHits(const CRay& transformedRay, unsigned objType)
 {
 //#pragma HLS INLINE
 #pragma HLS PIPELINE
@@ -91,10 +91,10 @@ myType PerformHits(const vec4* transformedRayData, unsigned objType)
 	switch(objType)
 	{
 	case SPHERE:
-		res = SphereTest(transformedRayData);
+		res = SphereTest(transformedRay);
 		break;
 	case PLANE:
-		res = PlaneTest(transformedRayData);
+		res = PlaneTest(transformedRay);
 		break;
 	default:
 		break;
@@ -103,29 +103,33 @@ myType PerformHits(const vec4* transformedRayData, unsigned objType)
 	return res;
 }
 
-#define OBJ_NUM 10
-#define WIDTH 200
-#define HEIGHT 200
-
-void InnerLoop(const CCamera& camera, const vec4& posShift, vec4* loadedRayData, const vec4* mats, const unsigned* objType,
-		unsigned w,
-		pixelColorType* frameBuffer)
+void InnerLoop(const CCamera& camera,
+				const vec4& posShift,
+				const vec4* mats,
+				const unsigned* objType,
+				unsigned w,
+				pixelColorType* frameBuffer)
 {
 #pragma HLS INLINE
 
 	myType currentClosest;
 	int currentObjIdx;
-	vec4 transformedRayData[2];
-#pragma HLS ARRAY_PARTITION variable=transformedRayData complete dim=1
+
+	vec4 loadedRayData[2];
+#pragma HLS ARRAY_PARTITION variable=loadedRayData complete dim=1
+//	vec4 transformedRayData[2];
+//#pragma HLS ARRAY_PARTITION variable=transformedRayData complete dim=1
 	vec4 mat[3];
-#pragma HLS ARRAY_PARTITION variable=mat complete dim=1
+#pragma HLS ARRAY_PARTITION variable=mat cyclic factor=3 dim=1
+
+	CRay ray, transformedRay;
 
 	InnerLoop: for (unsigned h = 0; h < HEIGHT; ++h)
 	{
 //#pragma HLS DATAFLOW
 #pragma HLS PIPELINE
 DO_PRAGMA(HLS UNROLL factor=OUTER_LOOP_UNROLL_FACTOR)
-		CreateRay(camera, posShift, w, h, loadedRayData);
+		CreateRay(camera, posShift, w, h, ray);
 		currentClosest = myType(-1);
 		currentObjIdx = -1;
 
@@ -135,8 +139,8 @@ DO_PRAGMA(HLS UNROLL factor=OUTER_LOOP_UNROLL_FACTOR)
 //#pragma HLS UNROLL
 #pragma HLS PIPELINE
 			AssignMatrix(mats, mat, n);
-			TransformRay(mat, loadedRayData, transformedRayData);
-			myType res = PerformHits(transformedRayData, objType[n]);
+			TransformRay(mat, ray, transformedRay);
+			myType res = PerformHits(transformedRay, objType[n]);
 
 			if (res != myType(-1) && res < currentClosest)
 			{
@@ -148,14 +152,10 @@ DO_PRAGMA(HLS UNROLL factor=OUTER_LOOP_UNROLL_FACTOR)
 //		color[1] = currentObjIdx;
 //		color[2] = currentClosest;
 
-//		frameBuffer[(w * HEIGHT + h) * 3 + 0] = color[0];
-//		frameBuffer[(w * HEIGHT + h) * 3 + 1] = color[1];
-//		frameBuffer[(w * HEIGHT + h) * 3 + 2] = color[2];
-
+		// TODO: The final result will consist of RGBA value (32 bit)
 		frameBuffer[h * 3 + 0] = currentClosest;
-		frameBuffer[h * 3 + 1] = currentObjIdx;
-		frameBuffer[h * 3 + 2] = currentObjIdx;
-
+//		frameBuffer[h * 3 + 1] = currentObjIdx;
+//		frameBuffer[h * 3 + 2] = currentObjIdx;
 	}
 }
 
@@ -183,17 +183,12 @@ int FFCore(const vec4* dataIn, unsigned dataInSize,
 				  myType(-0.5) * (HEIGHT - myType(1.0)),
 				  myType(0.0));
 
-	vec4 loadedRayData[2], transformedRayData[2], vecColor;
-#pragma HLS ARRAY_PARTITION variable=loadedRayData complete dim=1
-#pragma HLS ARRAY_PARTITION variable=transformedRayData complete dim=1
-#pragma HLS ARRAY_PARTITION variable=vecColor complete dim=1
-
 	vec4 mats[OBJ_NUM * 3];
 // WARNING: MAKES THINGS BLAZINGLY FAST
-#pragma HLS ARRAY_PARTITION variable=mats complete dim=1
+//#pragma HLS ARRAY_PARTITION variable=mats complete dim=1
 	memcpy(mats, dataIn, sizeof(vec4) * 3 * OBJ_NUM);
 
-	pixelColorType color[3], frameBuffer[3 * HEIGHT];
+	pixelColorType color[3], frameBuffer[FRAME_BUFFER_SIZE];
 #pragma HLS ARRAY_PARTITION variable=color complete dim=1
 //#pragma HLS ARRAY_PARTITION variable=frameBuffer complete dim=1
 	myType currentClosest;
@@ -206,13 +201,11 @@ int FFCore(const vec4* dataIn, unsigned dataInSize,
 	for (unsigned w = 0; w < WIDTH; ++w)
 	{
 #pragma HLS DATAFLOW
-		InnerLoop(camera, posShift, loadedRayData, mats, objType, w, frameBuffer);
+		InnerLoop(camera, posShift, mats, objType, w, frameBuffer);
 
-		//TODO -> Change output color to RGBA (4x8b) unsigned -> will reduce copy time by 3
-		memcpy(outColor, frameBuffer, sizeof(pixelColorType) * 3 * HEIGHT);
+		//TODO: Change output color to RGBA (4x8b) unsigned -> will reduce copy time by 3
+		memcpy(outColor, frameBuffer, sizeof(pixelColorType) * FRAME_BUFFER_SIZE);
 	}
-
-//	memcpy(outColor, frameBuffer, sizeof(pixelColorType) * 3 * WIDTH * HEIGHT);
 
 	return 0;
 
